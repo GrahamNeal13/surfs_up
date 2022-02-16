659e41ec1aaf971dc1cb7215bcd79d9350b47699
GrahamNeal13 <91097552+GrahamNeal13@users.noreply.github.com>
2022-02-16 14:18:52 -0700

    flask formatted

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
 #app.py
-from flask import Flask
+
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+session = Session(engine)
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello world'
-
+def welcome():
+    return(
+        f"Welcome to the Hawaii Climate Analysis API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/temp/start/end"
+    )
